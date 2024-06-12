59182eadc298ad04ad7975646836ecb9272bfc3d
Udit Karode <udit.karode@gmail.com>
2024-06-12 11:25:29 +0530

    fix: massively reduce `initialDelay`

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,7 +15,7 @@ export default function Home() {
 	const [disableAnimations, setDisableAnimations] =
 		useContext(animationContext);
 
-	const initialDelay = 0.7;
+	const initialDelay = 0.2;
 
 	const timings = {
 		line: { duration: 0.3, delay: initialDelay, type: "spring" },
