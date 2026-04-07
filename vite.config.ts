26c198468ce71ccf30d1cefcf252e718b5eaeb08
Udit Karode <udit.karode@gmail.com>
2026-04-08 07:50:27 +0900

    fix: remove now obsolete `vite-tsconfig-paths`

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import wyw from '@wyw-in-js/vite';
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+	resolve: {
+		tsconfigPaths: true,
+	},
 	plugins: [
-		tsconfigPaths(),
 		react(),
 		svgr({
 			svgrOptions: {
